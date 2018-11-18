afdef62ab6c5ba856b4e16c16ed542eebdaf4bb9
Dariusz Urbanczyk <dariusz.urbanczyk@calltracks.com>
2018-11-18 21:32:26 +0000

    Add httparty + few basic methods and tests

--- app/models/currency_cloud.rb
+++ app/models/currency_cloud.rb
@@ -1,17 +1,40 @@
 class CurrencyCloud
+  include HTTParty
+  format :json
+
+  base_uri 'https://coolpay.herokuapp.com'
+
+  def initialize
+    self.class.headers 'Content-type':  "application/json"
+  end
 
   def authenticate
-    username = "DariuszU"
-    api_key  = "63830AEF4DBFAD18"
+    self.class.post('/api/login', body: { "username": Rails.application.credentials.currency_cloud[:username],
+      "apikey": Rails.application.credentials.currency_cloud[:api_key]}.to_json )
   end
 
-  def add_recipent
+  def create_recipent(name)
+    response = with_token { self.class.post('/api/recipients', body: {recipient: {name: name}}.to_json) }
+    JSON.parse(response.body)["recipient"]["id"]
   end
 
-  def send_money
+  def send_money(recipient_id = nil, amount = nil, currency = "GBP")
+    with_token { self.class.post("api/payments", body: {payment: {amount: amount, currency: currency, recipient_id: recipient_id}}.to_json) }
   end
 
-  def check_payment
+  def check_payments
+    with_token { self.class.get("/api/payments", body: ''.to_json)["payments"] }
   end
+
+  def token
+    @token ||= JSON.parse(authenticate.response.body)["token"]
+  end
+
+  private
+
+    def with_token(&block)
+      self.class.headers 'Authorization': "Bearer #{token}"
+      block.call
+    end
 
 end
--- spec/models/currency_cloud_spec.rb
+++ spec/models/currency_cloud_spec.rb
@@ -3,14 +3,30 @@ require 'rails_helper'
 RSpec.describe CurrencyCloud, :type => :model do
 
   context "authenticate" do
-    it "has confirmation_token" do
-      expect(true).to be_truthy
+    it "should authenticate and return httparty response" do
+      VCR.use_cassette("currency_cloud/authenticate") do
+        expect(CurrencyCloud.new.authenticate.class.name).to eq("HTTParty::Response")
+      end
+    end
+
+    it "should return 200 if response was successful" do
+      VCR.use_cassette("currency_cloud/authenticate") do
+        expect(CurrencyCloud.new.authenticate.code).to eq(200)
+      end
+    end
+
+    it "should return in response token" do
+      VCR.use_cassette("currency_cloud/authenticate") do
+        expect(CurrencyCloud.new.authenticate.body.include?("token")).to be_truthy
+      end
     end
   end
 
-  context "add_recipent" do
-    it "has confirmation_token" do
-      expect(true).to be_truthy
+  context "create_recipent" do
+    it "should create the recipent with name john and return his recipent id" do
+      VCR.use_cassette("currency_cloud/create_new_recipent_john") do
+        expect(CurrencyCloud.new.create_recipent("john")).to eq("27434033-c653-4fe0-b80c-1d5ac37abbee")
+      end
     end
   end
 
@@ -20,9 +36,11 @@ RSpec.describe CurrencyCloud, :type => :model do
     end
   end
 
-  context "check_payment" do
+  context "check_payments" do
     it "has confirmation_token" do
-      expect(true).to be_truthy
+      VCR.use_cassette("currency_cloud/check_empty_payments") do
+        expect(CurrencyCloud.new.check_payments).to eq([])
+      end
     end
   end
 
